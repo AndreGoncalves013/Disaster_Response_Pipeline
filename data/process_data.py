bc04f6debc48f32ee67ec951df03c58b3327349d
André Gonçalves <andredesouzag@gmail.com>
2022-02-28 14:05:09 -0300

    Developed the process_data.py file

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,71 @@
 import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    '''
+    INPUT
+    messages_filepath - the file path to the disaster_messages.csv' file
+    categories_filepath - the file path to the disaster_categories.csv' file
+        
+    OUTPUT
+    df - Single dataframe with features and target columns for each category
+    '''
+
+    # Reading the csv files
+    messages_df = pd.read_csv('disaster_messages.csv')
+    categories_df = pd.read_csv('disaster_categories.csv')
+
+    # Merging messages with categories dataframes
+    df = messages_df.merge(categories_df, how='inner', on='id')
+
+    # Creating the target dataframe with each category column
+    target_df = df.categories.str.split(";", expand=True)
+    category_colnames = target_df.iloc[0,].apply(lambda x:x[:-2])
+    target_df.columns = category_colnames
+
+    # Converting category values to numbers 0 or 1
+    for column in target_df:
+       
+        target_df[column] = target_df[column]\
+            .apply(lambda x:x[-1])\
+            .astype(int)
+    
+    # Concatenating the original df with the target_df
+    df = df.drop(columns='categories', axis=1)
+    df = pd.concat([df, target_df], axis=1)
+
+    return df
+    
+
 
 
 def clean_data(df):
-    pass
+    '''
+    INPUT
+    df - Pandas Dataframe that might contain duplicates
+        
+    OUTPUT
+    cleaned_df - Dataframe after doing all cleaning process
+    '''
+
+    cleaned_df = df.drop_duplicates()
+    return cleaned_df
 
 
 def save_data(df, database_filename):
-    pass  
+
+    '''
+    INPUT
+    df - Dataframe that will be loaded
+    database_filename - Database where the data will be loaded
+    '''
+
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('disaster_messages', engine, index=False)
 
 
 def main():
